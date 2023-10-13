bcb9a999f0b2a9da8c05e6f46739c0191dd4989f
Mike-D22 <qamp26@gmail.com>
2023-10-13 02:30:37 -0700

    New Updates

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module creates a User class.
+"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Class for managing city objects.
+    """
+
+    state_id = ""
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module, creates User class
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Class for managing state objects
+    """
+
+    name = ""
