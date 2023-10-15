1bc7eaa75e2417e846ad264fec5b2d7055d027e2
Mike-D22 <qamp26@gmail.com>
2023-10-15 07:45:35 -0700

    New update

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This modules creates a Amenity class
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class for managing amenity objects
+    """
+
+    name = ""
